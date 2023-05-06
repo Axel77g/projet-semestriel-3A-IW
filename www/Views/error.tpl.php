538aeb0f6813a16072b8c91bce6244500b6f4ec5
Prumme <aurelien23.p@gmail.com>
2023-05-06 18:08:45 +0200

    :pencil: style des erreurs

--- www/Views/error.tpl.php
+++ www/Views/error.tpl.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/src/css/style.css">
     <title>ERROR Template</title>
 </head>
+
 <body>
-    <h1> Error <?= $this->data['code'] ?> : <?= $this->data['message'] ?></h1>
+    <div class="error">
+        <h1> Error
+            <?= $code ?>
+        </h1>
+        <h2>
+            <?= $message ?>
+        </h2>
+
+        <a href="/">
+            Revenir en lieu sûr
+        </a>
+    </div>
 </body>
+
 </html>
