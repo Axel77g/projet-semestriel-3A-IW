11a28a96632b110a6ac41c88ac301abb42362aa4
Axel G <41450822+Axel77g@users.noreply.github.com>
2023-05-06 09:11:11 +0200

    Update Main.php

--- www/Controllers/Main.php
+++ www/Controllers/Main.php
@@ -8,11 +8,11 @@ use App\Core\View;
 class Main extends Controller{
     public function index(){
         $view = new View("Main/index","front");
-        $view->asign("name","<h3>Index Controller ta darone Robin</h3>");
+        $view->asign("name","<h3>Index Controller</h3>");
     }
     public function contact(){
         $view = new View("Main/index","front");
-        $view->asign("name","<h3>Contact Controller ta darone Robin</h3>");
+        $view->asign("name","<h3>Contact Controller</h3>");
 
     }
 }
