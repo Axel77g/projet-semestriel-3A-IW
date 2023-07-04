4d5238f3ed89fa61878a6a9354dbbb0ece05b845
Axel G <41450822+Axel77g@users.noreply.github.com>
2023-07-04 20:41:27 +0200

    Update view.js

--- www/src/js/templates/article_list/view.js
+++ www/src/js/templates/article_list/view.js
@@ -2,6 +2,6 @@ import Component from "../../core/Component.js";
 
 export class ArtilceListView extends Component {
   render() {
-    return createElement("h1", {}, "Artilce View");
+    return createElement("h1", {}, "Article View");
   }
 }
