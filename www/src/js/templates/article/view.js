96ad52d58637bb9aa2ec60a03a1f81a36b489588
Axel <axel@reality-academy.fr>
2023-07-04 18:19:01 +0200

    [TEMPLATE] create page template files

---
+++ www/src/js/templates/article/view.js
@@ -0,0 +1,7 @@
+import Component from "../../core/Component.js";
+
+export class ArticleView extends Component {
+  render() {
+    return createElement("h1", {}, "Article View");
+  }
+}
---
+++ www/src/js/templates/home/view.js
@@ -0,0 +1,7 @@
+import Component from "../../core/Component.js";
+
+export class HomeView extends Component {
+  render() {
+    return createElement("h1", {}, "Home View");
+  }
+}
