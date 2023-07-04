72b232c08223601f650214fc902599eb637350f7
Axel <axel@reality-academy.fr>
2023-07-04 18:19:32 +0200

    [PAGE EDIT] create page and route for the page edition

---
+++ www/src/js/pages/Backoffice/PageEdit.js
@@ -0,0 +1,74 @@
+import Button from "../../components/ui/Button.js";
+import Input from "../../components/ui/Input.js";
+import Select from "../../components/ui/Select.js";
+import Component from "../../core/Component.js";
+import { createElement } from "../../core/Element.js";
+
+/* templates */
+import {
+  HomeEdit,
+  ArticleEdit,
+  ArticleListEdit,
+} from "../../templates/index.js";
+
+export default class PageEdit extends Component {
+  init() {
+    this.state = {
+      template: -1,
+      typesOptions: [
+        {
+          label: "Home",
+          value: "Home",
+        },
+        {
+          label: "Article",
+          value: "Article",
+        },
+        {
+          label: "Articles list",
+          value: "ArticleList",
+        },
+      ],
+    };
+  }
+
+  get editComponent() {
+    if (this.state.template == -1) return null;
+    return eval(this.state.template + "Edit");
+  }
+
+  handleChange(e) {
+    console.log(e);
+    this.setState({
+      [e.name]: e.value,
+    });
+  }
+  handleSubmit(e) {
+    console.log("submit");
+  }
+  render() {
+    return createElement(
+      "div",
+      {},
+      [
+        createElement(Input, {
+          name: "title",
+          placeholder: "Titre de la page",
+          onChange: this.handleChange.bind(this),
+        }),
+        createElement(Select, {
+          options: this.state.typesOptions,
+          name: "template",
+          label: "Template de page",
+          placeholder: "Template de page",
+          onChange: this.handleChange.bind(this),
+        }),
+        this.editComponent ? createElement(this.editComponent, {}) : null,
+        createElement(Button, {
+          onClick: this.handleSubmit.bind(this),
+          children: "Enregistrer",
+        }),
+      ].filter(Boolean)
+    );
+  }
+}
