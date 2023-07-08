8bb48742a5a2733a175d6145b04994780c37484a
Axel <axel77g@hotmail.com>
2023-07-08 14:23:46 +0200

    [BOOSTRAP] fix the import style and JS

--- www/Views/front.tpl.php
+++ www/Views/front.tpl.php
@@ -6,17 +6,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/src/css/style.css">
-    <link rel="stylesheet" href="/src/assets/bootstrap.css" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="/src/assets/bootstrap/css/bootstrap.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="/src/assets/bootstrap/css/bootstrap-theme.min.css" crossorigin="anonymous">
+    
+    
     <title>Front Template</title>
-
 
 </head>
 
 <body>
-<script src="/src/assets/jquery.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="/src/assets/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="/src/assets/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <?php include $this->view; ?>
 </body>
+
+<script src="/src/assets/jquery.js"></script>
+<script src="/src/assets/bootstrap/js/bootstrap.min.js" ></script>
 
 </html>
