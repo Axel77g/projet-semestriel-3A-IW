c8fc4c68a48be008917d65ca195ffd7c49a29a98
Axel <axel77g@hotmail.com>
2023-07-09 22:41:22 +0200

    [REQUEST] create a query class to interact with $_GET

--- www/Core/Request.php
+++ www/Core/Request.php
@@ -37,6 +37,10 @@ class Request{
         return $this->headers;
     }
 
+    public function getQuery(){
+        return new RequestQuery($_GET);
+    }
+
     public function body(){
         return file_get_contents('php://input');
     }
@@ -53,4 +57,21 @@ class Request{
         }
         return null;
     }
+}
+
+
+class RequestQuery{
+    protected array $query = [];
+    public function __construct($query){
+        $this->query = $query;
+    }
+
+    public function get($key){
+        return $this->query[$key];
+    }
+
+    public function has($key){
+        return isset($this->query[$key]);
+    }
+
 }
