821c3b13b050d6e719a2a3c6ab7a95c91267e168
Prumme <aurelien23.p@gmail.com>
2023-07-17 21:58:29 +0200

    insert component inside Home component

--- www/src/js/components/Home.js
+++ www/src/js/components/Home.js
@@ -2,6 +2,8 @@ import Component from "../core/Component.js";
 import Api from "../core/Api.js";
 import Header from "./global/Header.js";
 
+import { HomeView as home } from "../templates/index.js";
+
 export default class Home extends Component {
   init() {
     document.title = "Blog";
@@ -10,17 +12,28 @@ export default class Home extends Component {
 
   async fetchHome() {
     const api = new Api();
-    const home = await api.get("api/pages/home");
+    const home = await api.get("api/pages/home?withContent");
+    if (!home) return;
+    this.setState({ page: home });
+  }
 
-    router.push(home.path);
+  get view() {
+    const template = this.state.page.template;
+
+    return eval(template);
+  }
+
+  get page() {
+    return this.state.page;
   }
 
   render() {
-    return createElement("div", { class: ["home"] }, [
-      createElement(Header, {}),
-      createElement("div", { class: ["container-fluid", "mt-4"] }, [
-        createElement("h1", {}, "Bienvenue sur Faraway - Blog"),
-      ]),
+    if (!this.state.page) return createElement("div", {}, "");
+    return createElement("div", {}, [
+      createElement(Header, {}, []),
+      createElement(this.view, { page: this.page }, []),
+      this.page.is_commentable &&
+        createElement(CommentConversation, { page: this.page }, []),
     ]);
   }
 }
