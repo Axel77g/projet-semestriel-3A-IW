fadc622d4828faed1ae86631c4ab23d2693d547f
Axel <axel77g@hotmail.com>
2023-07-19 18:07:13 +0200

    [FORGOT PASSWORD] fix display

--- www/src/js/pages/ResetPassword/ForgotPassword.js
+++ www/src/js/pages/ResetPassword/ForgotPassword.js
@@ -31,14 +31,18 @@ export default class ForgotPassword extends Component {
 
   render() {
     if (this.state.success === true) {
-      return createElement("div", { class: ["alert", "alert-success"] }, [
-        createElement("h1", { class: ["header"] }, "Email envoyé"),
-        createElement(
-          "p",
-          {},
-          "Merci de vérifier votre boîte mail pour réinitialiser votre mot de passe."
-        ),
-      ]);
+      return createElement(
+        "div",
+        { class: ["alert", "alert-success", "ma-5"] },
+        [
+          createElement("h1", { class: ["header"] }, "Email envoyé"),
+          createElement(
+            "p",
+            {},
+            "Merci de vérifier votre boîte mail pour réinitialiser votre mot de passe."
+          ),
+        ]
+      );
     } else {
       return createElement("div", { class: "box-wrapper" }, [
         createElement("h1", { class: ["header"] }, "Mot de passe oublié ?"),
